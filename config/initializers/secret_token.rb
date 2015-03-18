cdca2a1b43bfeea92a51fb270ad92c211f7cb1d4
Luigi <luigi.derussis@polito.it>
2015-03-18 12:55:47 +0100

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+PoliRun::Application.config.secret_token = 'f56a489b1c01715942762cbf438cbecec3a69e5084936f12efa04fc0be333013291456211e4b7ea57f3e382d15ce8898017627cdb6fe09d4ac619d2a48ff7591'
